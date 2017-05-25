fe35dc4e5c076fecf8bfca831024f0aa05c165a8
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-05-25 09:33:00 +0300

    no message

--- Lesson_14_Свойства.playground/Contents.swift
+++ Lesson_14_Свойства.playground/Contents.swift
@@ -4,18 +4,78 @@ struct Student {
     
     var firstName : String {
         
-        willSet(newFirstName) {
-            print("will set" + newFirstName + " instead of " + firstName)
+        willSet {
+            print("will set " + newValue + " instead of " + firstName)
         }
         
-        didSet(oldFirstName) {
-            print("did set" + firstName + " instead of " + oldFirstName)
+        didSet {
+            print("did set " + firstName + " instead of " + oldValue)
+            
+            firstName = firstName.capitalized
+        }
+    }
+    
+    var lastName : String {
+        didSet {
+            lastName = lastName.capitalized
+        }
+    }
+    
+    var fullName : String {
+        
+        get {
+            return firstName + " " + lastName
+        }
+        set {
+            print("fullName wants to be set to " + newValue)
+            
+            let words = newValue.components(separatedBy: " ")
+            if words.count > 0 {
+                self.firstName = words[0]
+            }
+            
+            if words.count > 1 {
+                self.lastName = words[1]
+            }
         }
     }
 }
 
 
-var student = Student(firstName: "Alex")
+var student = Student(firstName: "Alex", lastName: "Skutarenko")
+
 student.firstName
+student.lastName
+student.fullName
 
 student.firstName = "Bob"
+
+student.firstName
+student.lastName
+student.fullName
+
+student.fullName = "Ivan Ivanov"
+student.firstName
+student.lastName
+student.fullName
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
