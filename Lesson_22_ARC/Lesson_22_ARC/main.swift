d3d773007fb163445d8c6f50976f25df052211bf
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-05-30 11:45:20 +0300

    no message

--- Lesson_22_ARC/Lesson_22_ARC/main.swift
+++ Lesson_22_ARC/Lesson_22_ARC/main.swift
@@ -1,12 +1,101 @@
-//
-//  main.swift
-//  Lesson_22_ARC
-//
-//  Created by Stanly Shiyanovskiy on 30.05.17.
-//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
-//
 
-import Foundation
+var playground = true
 
-print("Hello, World!")
+class Student {
+    
+    unowned var teacher : Teacher
+    
+    deinit {
+        print("goodbye student")
+    }
+    
+    init(teacher: Teacher) {
+        self.teacher = teacher
+    }
+}
+
+class Teacher {
+    
+    var student : Student!
+    
+    var test : (() -> ())?
+    
+    lazy var test2 : (Bool) -> () = {
+        [unowned self] (a: Bool) in
+        print(self.student)
+        //print(self)
+    }
+    
+    init() {
+        student = Student(teacher: self)
+    }
+    
+    deinit {
+        print("goodbye teacher")
+    }
+}
+
+var closure : (() -> ())?
+if playground {
+    
+    //var t : Teacher?
+    var teacher = Teacher()
+    
+    teacher.test2(true)
+    
+//    teacher.test = {
+//        [unowned teacher] in
+//        print(teacher)
+//    }
+    
+    
+//    if playground {
+//        
+//        var student = Student(teacher: teacher)
+//        teacher.student = student
+//        
+//    }
+    
+    var student = teacher.student
+    closure = {
+        [weak student] in
+        print(student ?? 0)
+    }
+    
+    print("exit playground")
+}
+
+print("end")
+
+//********************************
+
+//var x = 10
+//var y = 20
+//
+//class Human {
+//    var name  = "a"
+//}
+//
+//var h = Human()
+//
+//var closure3 : () -> () = {
+//    [x] in
+//    print("\(x) \(y)")
+//}
+//
+//var closure2 : (Int) -> (Int) = {[x,y,h] (a: Int) -> Int in
+//    print("\(x) \(y) \(h.name)")
+//    return a
+//}
+//
+//closure()
+//closure2(1)
+//
+//x = 30
+//y = 40
+//h = Human()
+//h.name = "b"
+//
+//closure()
+//closure2(1)
 
