f1250b1e1b146d4221eecd8792698f2b45b445dd
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-05-30 10:49:58 +0300

    no message

--- Lesson_21_Инициализаторы часть 3.playground/Contents.swift
+++ Lesson_21_Инициализаторы часть 3.playground/Contents.swift
@@ -1,3 +1,137 @@
 import UIKit
 
+class Human {
+    var weight : Int
+    var age : Int
+    
+    init(weight: Int, age: Int) {
+        self.weight = weight
+        self.age = age
+    }
+    
+    convenience init(age: Int) {
+        self.init(weight: 0, age: age)
+    }
+    
+    convenience init(weight: Int) {
+        self.init(weight: weight, age: 0)
+    }
+    
+    convenience init() {
+        self.init(weight: 0)
+    }
+    
+    func test() {
+        
+    }
+    
+    deinit {
+        print("Human deinitialized")
+    }
+}
+
+enum Color : Int {
+    case Black
+    case White
+    
+    init?(_ value: Int) {
+        switch value {
+        case 0: self = .Black
+        case 1: self = .White
+        default: return nil
+        }
+    }
+}
+
+let a = Color(-1)
+a?.rawValue
+
+struct Size {
+    var width : Int
+    var height : Int
+    
+    init?(width: Int, height: Int) {
+        self.width = width
+        if height < 0 {
+            return nil
+        }
+        self.height = height
+    }
+}
+
+class Friend : Human {
+    var name : String!
+    
+    var skin : Color = {
+        let random = Int(arc4random_uniform(2))
+        return Color(random)!
+    }()
+    
+    init?(name: String) {
+        self.name = name
+        super.init(weight: 0, age: 0)
+        if name.isEmpty {
+            return nil
+        }
+    }
+    
+    required init() {
+        self.name = "Hi"
+        super.init(weight: 0, age: 0)
+    }
+    
+    deinit {
+        print("Friend deinitialized")
+    }
+}
+
+let f = Friend(name: "")
+f?.name
+
+class BestFriend : Friend {
+    
+    override init(name: String) {
+        if name.isEmpty {
+            super.init()
+        } else {
+            super.init(name: name)!
+        }
+    }
+    
+    required init() {
+        super.init()
+    }
+    
+    deinit {
+        print("BestFriend deinitialized")
+    }
+}
+
+let bF = BestFriend(name: "")
+bF.name
+
+let f1 = Friend(name: "a")
+f1!.skin
+let f2 = Friend(name: "b")
+f2!.skin
+let f3 = Friend(name: "c")
+f3!.skin
+let f4 = Friend(name: "d")
+f4!.skin
+
+struct Test {
+    var bestFriend : BestFriend? = BestFriend(name: "")
+}
+
+var test : Test? = Test()
+
+test!.bestFriend = nil
+
+var f5 = Friend(name: "")
+
+
+
+
+
+
 
---
+++ Lesson_22_ARC/Lesson_22_ARC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson_22_ARC
+//
+//  Created by Stanly Shiyanovskiy on 30.05.17.
+//  Copyright © 2017 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
