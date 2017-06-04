e907805acfd663eb435e85013fc69389d7b3f651
Stanly Shiyanovskiy <st-small@rambler.ru>
2017-06-04 19:20:42 +0300

    no message

--- Lesson_27_HW.playground/Contents.swift
+++ Lesson_27_HW.playground/Contents.swift
@@ -12,6 +12,161 @@ import UIKit
  5. Не все, что мы кладем в холодильник, является едой. Поэтому сделайте так, чтобы Storable не наследовался от Food. Мы по прежнему приносим еду домой, но некоторые продукты реализуют теперь 2 протокола. Холодильник принимает только те продукты, которые еще и Storable. функция сортировки должна по прежнему работать.
  */
 
+protocol Things {
+    var name: String { get }
+}
+
+protocol Food {
+    var name: String { get }
+    func taste()
+}
+
+protocol Storable: Things {
+    var expired: Bool { get }
+    var daysToExpire: Int { get }
+    func putToFridge(_ text: String)
+}
+
+class Bread: Food {
+    var name: String
+    
+    func taste() {
+        print("mmm \(name) is important product!")
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Milk: Storable, Food {
+    var name: String
+    var expired: Bool {
+        return daysToExpire == 0 ? false : true
+    }
+    var daysToExpire: Int
+    func taste() {
+        print("mmm \(name) is a cow milk, - it's tasty great!")
+    }
+    
+    func putToFridge(_ text: String) {
+        print(text)
+    }
+    
+    init(name: String, daysToExp: Int) {
+        self.name = name
+        self.daysToExpire = daysToExp
+    }
+}
+
+class Eggs: Storable, Food {
+    var name: String
+    var expired: Bool {
+        return daysToExpire == 0 ? false : true
+    }
+    var daysToExpire: Int
+    func taste() {
+        print("mmm \(name) includes so much of proteins, - it is excellent to breakfast!")
+    }
+    func putToFridge(_ text: String) {
+        print(text)
+    }
+    
+    init(name: String, daysToExp: Int) {
+        self.name = name
+        self.daysToExpire = daysToExp
+    }
+}
+
+class Butter: Storable, Food {
+    var name: String
+    var expired: Bool {
+        return daysToExpire == 0 ? false : true
+    }
+    var daysToExpire: Int
+    func taste() {
+        print("mmm \(name) tastes good, but not so much!")
+    }
+    func putToFridge(_ text: String) {
+        print(text)
+    }
+    
+    init(name: String, daysToExp: Int) {
+        self.name = name
+        self.daysToExpire = daysToExp
+    }
+}
+
+func random() -> Int {
+    return Int(arc4random_uniform(5))
+}
+
+let bread = Bread(name: "Бородинский хлеб")
+let milk = Milk(name: "Пастеризированное молоко", daysToExp: random())
+let eggs = Eggs(name: "Яйца куриные", daysToExp: random())
+let butter = Butter(name: "Масло селянское", daysToExp: random())
+let milk2 = Milk(name: "Пастеризированное молоко2", daysToExp: random())
+let eggs2 = Eggs(name: "Яйца куриные2", daysToExp: random())
+let butter2 = Butter(name: "Масло селянское2", daysToExp: random())
+let milk3 = Milk(name: "Пастеризированное молоко3", daysToExp: random())
+let eggs3 = Eggs(name: "Яйца куриные3", daysToExp: random())
+let butter3 = Butter(name: "Масло селянское3", daysToExp: random())
+
+var productsArray: [Food] = [bread, milk, eggs, butter, milk2, eggs2, butter2, milk3, eggs3, butter3]
+
+func sortMyArr(_ array: inout Array<Food>) {
+        
+    array.sort{
+        return $0.name.lowercased() < $1.name.lowercased()
+    }
+}
+
+func sortMyArr2(_ array: inout Array<Storable>) {
+    array.sort(by: {a, b in
+        if a.daysToExpire == b.daysToExpire {
+            return a.name.lowercased() < b.name.lowercased()
+        } else {
+            return a.daysToExpire < b.daysToExpire
+        }
+    })
+}
+
+func printBagContains(_ array: [Food]) {
+
+    var arrayV = array
+    sortMyArr(&arrayV)
+    
+    for value in arrayV {
+        value.taste()
+    }
+}
+
+func printFridgeContains(_ array: [Food]) {
+    var arr: [Storable] = []
+    
+    for value in array {
+        if let product = value as? Storable {
+            arr.append(product)
+        }
+    }
+    
+    sortMyArr2(&arr)
+    for product in arr {
+        if product.expired {
+        product.putToFridge("Just put \(product.name) in fridge. It will expire in \(product.daysToExpire) days!")
+        } else {
+            print("\(product.name) expired and I'll throw out it!")
+        }
+    }
+}
+
+print("There are in my bag: ")
+printBagContains(productsArray)
+
+print("\nThere are in my fridge: ")
+printFridgeContains(productsArray)
+
+
 
 
 
